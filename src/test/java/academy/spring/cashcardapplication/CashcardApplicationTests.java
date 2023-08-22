b7513bafc7647684f98bfc4be18ff41075cd4edb
brwngda <grz.barwinski@gmail.com>
2023-08-22 16:16:32 +0200

    first commit

---
+++ src/test/java/academy/spring/cashcardapplication/CashcardApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.spring.cashcardapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashcardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
